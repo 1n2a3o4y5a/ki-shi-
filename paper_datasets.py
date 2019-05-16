61ba030c4727e18c9fe25dd441f5b58f58cfaf26
kernel0922 <user.id.125125@gmail.com>
2019-05-16 16:24:18 +0900

    k-meansで分類、F1スコアを算出

--- paper_datasets.py
+++ paper_datasets.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn import cluster
+from sklearn.cluster import KMeans
+from sklearn.metrics import confusion_matrix
 
+#乱数の作成
 np.random.seed(0)
 array1 = np.random.normal(0,1,(10000,3))
 array2 = np.random.normal(0,1,(10000,3))
@@ -9,6 +13,14 @@ array3 = np.random.normal(0,24,(1,3))
 array2[:501] = array2[:501] + array3
 experiment = array2
 
+#実験群をデータフレーム化
+target = pd.DataFrame(np.zeros(10000))
+target[:500] = 1
+target[501:] = 0
+df_experiment = pd.DataFrame(experiment)
+df_experiment["target"] = target
+
+#ヒストグラム
 plt.subplot(3,1,1)
 plt.title("array1")
 plt.hist(array1)
@@ -18,8 +30,24 @@ plt.hist(array3)
 plt.subplot(3,1,3)
 plt.title("experiment")
 plt.hist(experiment)
-plt.show()
+#plt.show()
 
-heat_map = np.corrcoef(experiment[:501])
-heat_map2 = np.mean(heat_map - np.diag(np.diag(heat_map)))
-print(heat_map2)
+#k-means
+e = cluster.KMeans(n_clusters=2)
+e.fit(experiment)
+predict = KMeans(n_clusters=2).fit_predict(df_experiment)
+pre = []
+for i in predict:
+    pre.append(i)
+matrix = confusion_matrix(df_experiment["target"].values.tolist(), pre)
+print(matrix)
+
+#Fスコア計算
+TP = matrix[1,1]
+FP = matrix[0,1]
+FN = matrix[1,0]
+P = TP + FN
+precision = TP / (TP + FP)
+recall = TP / P
+F1 = 2 / (1 / precision + 1 / recall)
+print(F1)
